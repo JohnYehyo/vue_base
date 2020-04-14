c32e213fee300eef7651fa1d09a320b296131d09
JohnYehyo <836524969@qq.com>
2020-04-14 21:34:34 +0800

    路由导航守卫控制token访问

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,18 @@ const router = new VueRouter({
   routes
 });
 
+//挂载路由导航守卫
+
+router.beforeEach((to, from, next) => {
+  if (to.path == "/login") {
+    return next();
+  }
+  //获取token
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (!tokenStr) {
+    return next("/login");
+  }
+  next();
+});
+
 export default router;
