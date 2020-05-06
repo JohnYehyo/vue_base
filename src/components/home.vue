726aee98e84b7c93f2c36680f0e2fc6e7c5d27f6
JohnYehyo <836524969@qq.com>
2020-05-06 22:01:50 +0800

    主页

--- src/components/home.vue
+++ src/components/home.vue
@@ -34,18 +34,6 @@
               </el-menu-item>
             </el-submenu>
           </el-submenu>
-          <!-- <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>-->
         </el-menu>
       </el-aside>
       <el-main>Main</el-main>
@@ -71,9 +59,7 @@ export default {
 
     },
     async menus(){
-   
      const result =  await this.$http.get("sys/menu");
-     console.log(result)
     }
   }
 };
